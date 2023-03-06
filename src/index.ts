3b0c8c8304321f5f1c495a274447cf0a036df1b8
Naimur Islam <90306726+Naiml007@users.noreply.github.com>
2023-03-06 12:00:39 +0600

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import Eris from "eris";
+
+import pingCommand from "./commands/ping";
+
+import helloCommand from "./commands/hello";
+
+
+const bot = Eris(`Bot ${process.env.DISCORD_TOKEN}`);
+
+bot.on("ready", () =>
+
+	console.log(`Logged in as ${bot.user.username}#${bot.user.discriminator}`));
+
+
+bot.on("messageCreate", (message) => {
+
+  if (message.content.startsWith("!")) {
+
+    // Parse the command from the message content
+
+    const [command, ...args] = message.content.slice(1).split(" ");
+
+    // Handle the command based on its name
+
+    switch (command) {
+
+      case "ping":
+
+        pingCommand(bot, message, args);
+
+        break;
+
+      case "hello":
+
+        helloCommand(bot, message, args);
+
+        break;
+
+      default:
+
+        bot.createMessage(message.channel.id, "Unknown command.");
+
+        break;
+
+    }
+
+  }
+
+});
+
+bot.connect();
+
